4f3fbb45f0146eb41770f830d46db71891ee15eb
Amanuel2000 <amanuelf44@gmail.com>
2023-03-11 21:56:27 +0000

    quick fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,8 @@
-# Fix nginx to many failed requests
-exec {'increase_workers':
-command => '/bin/sed -i "s/worker_processes 4/worker_processes 8/g" /etc/nginx/nginx.conf',
-notify  => Service['nginx'],
-}
-
-service {'nginx':
-ensure => 'running',
+# Changes the limitations on the holberton user
+exec { 'change-os-configuration-for-holberton-user':
+  command => "bash -c \"sed -iE 's/^holberton hard nofile \
+5/holberton hard nofile 88888/' /etc/security/limits.conf; \
+sed -iE 's/^holberton soft nofile \
+4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
